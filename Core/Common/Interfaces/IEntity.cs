4f7afc74168ac01c76e54cfeb0d873e80e306c3e
capkirk45 <kmiller@zhealthdocumentation.com>
2018-01-08 20:55:05 -0800

    Updates to DataAccess layer

--- Core/Common/Interfaces/IEntity.cs
+++ Core/Common/Interfaces/IEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WF.Common.Interfaces
+﻿namespace WF.Common.Interfaces
 {
     public interface IEntity
     {
---
+++ Core/Data/Models/ForecastModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WF.DataAccess.Models
+{
+    using Common.Entities;
+    using System;
+    using System.Data.Common;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public partial class ForecastModel : DbContext
+    {
+        public ForecastModel(DbConnection sqlconnection) : base(sqlconnection, false)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+           public virtual DbSet<Forecast> Forecasts { get; set; }
+    }
+}
